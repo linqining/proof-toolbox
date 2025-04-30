57622cd23ce0c3d9d4eacc17f290ec950fccb7c9
linqining <linqining@outlook.com>
2025-05-01 05:51:30 +0800

    feat:remove debug

--- proof-essentials/src/zkp/arguments/shuffle/proof.rs
+++ proof-essentials/src/zkp/arguments/shuffle/proof.rs
@@ -13,7 +13,7 @@ use ark_serialize::{CanonicalDeserialize, CanonicalSerialize, SerializationError
 use ark_std::io::{Read, Write};
 use digest::Digest;
 
-#[derive(CanonicalDeserialize, CanonicalSerialize,Debug)]
+#[derive(CanonicalDeserialize, CanonicalSerialize)]
 pub struct Proof<Scalar, Enc, Comm>
 where
     Scalar: Field,
